2fb801f76c702891ccfbbc3f0bb1c80c5c6e1cf5
Andrii Maliuta <quadr988@gmail.com>
2022-01-22 18:10:55 +0200

    Web - /persons + cors for react native

--- web/web.go
+++ web/web.go
@@ -45,6 +45,10 @@ func UseMiddlewares(h http.Handler) http.Handler {
 	//h = Cors(h)
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("%s: %s", r.Method, r.RequestURI)
+		w.Header().Add("Access-Control-Allow-Origin", "*")
+		//w.Header().Add("Access-Control-Allow-Credentials", "true")
+		//w.Header().Add("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		//w.Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		h.ServeHTTP(w, r)
 	})
 }
